f0e0221a0228967c0997ce9de548c286e0953660
SPNicolas <nicolassteffenspereira@gmail.com>
2024-02-23 15:09:51 -0300

    transformando o contador

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,30 @@ import { ref } from 'vue';
 
 const contador = ref(0);
 
-const pessoa = {
-  nome: 'Nicolas',
-  cidade: 'Joinville'
-};
+function decrementar() {
+  contador.value--
+}
+function resetar() {
+  contador.value = 0
+}
 
-pessoa.nome = 'Novo nome';
+if (contador < 0) {
+  
+}
 </script>
 
 <template>
   <button @click="contador++">+</button>
-  <button @click="contador--">-</button>
+  <button @click="decrementar">-</button>
+  <button @click="resetar">zerar</button>
   <p>Valor do contador: {{  contador }}</p>
 </template>
 
 <style scoped>
-
+button{
+  background-color: black;
+  color: aliceblue;
+  margin: 5pxe;
+  width: 100px;
+}
 </style>
